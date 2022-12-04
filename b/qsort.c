2b9ef5bddf43e58225815cc25b8e7372fe671fe7
BhavyaBh289 <shahbhavyasp@gmail.com>
2022-12-04 13:04:54 +0530

    basic struct

--- b/qsort.c
+++ b/qsort.c
@@ -1,1 +1,20 @@
 #include<stdio.h>
+#include<malloc.h>
+struct node {
+    int data;
+    struct node * next;
+};
+int main(){
+    struct node * head = NULL;
+    int n,temp;
+    do{
+    printf("1 to add value 2 to show value 3to remove a value 4 to exit");
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+            printf("value to be added");
+            scanf("%d",&temp);
+            head = addnode(head,temp);
+    }
+    }while(n !=4)
+}
